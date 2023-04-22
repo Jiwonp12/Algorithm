b9c58b37f30cbcbc10758e97c373df30a1fa3851
Jiwonp12 <124653132+Jiwonp12@users.noreply.github.com>
2023-04-22 23:21:53 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -0,0 +1,12 @@
+function solution(citations) {
+    citations.sort((a, b) => b - a);
+    let h = 0;
+    for (let i = 0; i < citations.length; i++) {
+        if (i+1 <= citations[i]) {
+            h = i+1;
+        } else {
+            break;
+        }
+    } 
+    return h;
+}
