17e40b0e49b5a0c458c0f70ed20fc17e04fd94b3
Jiwonp12 <124653132+Jiwonp12@users.noreply.github.com>
2023-05-22 15:47:22 +0900

    [unrated] Title: n보다 커질 때까지 더하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -0,0 +1,13 @@
+function solution(numbers, n) {
+    let result = 0;
+    
+    for (let i = 0; i < numbers.length; i++) {
+        if (result <= n) {
+            result += numbers[i];
+        } else {
+            break;
+        }
+    }
+    
+    return result;
+}
