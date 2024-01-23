d9dc1bbc7b3a196acae0e2158bf34802dcb54046
Jiwonp12 <124653132+Jiwonp12@users.noreply.github.com>
2024-01-23 11:21:32 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    let result = [n];
+    let num = n;
+    
+    while (num !== 1) {
+        if (num % 2 === 0) {
+            num /= 2;
+        } else {
+            num = 3 * num + 1;
+        }
+        result.push(num);
+    }
+    return result;
+}
