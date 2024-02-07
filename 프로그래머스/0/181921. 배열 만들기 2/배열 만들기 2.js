b8beb344811737ddcda305781c89a1fca152351d
Jiwonp12 <124653132+Jiwonp12@users.noreply.github.com>
2024-02-08 00:41:38 +0900

    [level 0] Title: 배열 만들기 2, Time: 12.45 ms, Memory: 39.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.js
@@ -0,0 +1,10 @@
+function solution(l, r) {
+    let result = [];
+    
+    for (let i = l; i <= r; i++) {
+        if (String(i).split('').every(num => num === '0' || num === '5')) {
+            result.push(i);
+        }
+    }
+    return result.length !== 0 ? result : [-1];
+}
